76d5c6d98ca69bebf2f290d6f2a8433cee3f0530
Nikil-D-Gr8 <snikilpaul@gmail.com>
2024-09-25 23:49:23 +0530

    add : type casting for float nd double

---
+++ KK/PractiseProblems/src/Main.java
@@ -0,0 +1,6 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("We'll be doing Problems from the DSA-Bootcamp-Java by Kunal Kushwaha");
+    }
+}
---
+++ KK/Typecasting/src/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("We'll look at type conversions both broad and narrow ones.");
+    }
+}
